afefa09c933baf7e3b467657f1eadc33b46ff4d3
Yuliiiaaaa <as_ashes_00@mail.ru>
2025-04-02 14:05:18 +0300

    first commit

---
+++ ee.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img = mpimg.imread('C:\\Users\\user\\Desktop\\my_dataset\\result\\professionclassification.png')
+plt.imshow(img)
+plt.show()
